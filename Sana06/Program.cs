31653989947ac8fbe5510097c17272bba3e844f8
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2024-01-06 21:12:10 +0200

    Add code for code that calls the ShowInfo() method for each element of the "people" array

--- Sana06/Program.cs
+++ Sana06/Program.cs
@@ -4,23 +4,17 @@ static class Program
 {
     static void Main()
     {
-        DateTime date = new DateTime(2006, 9, 27);
-        Person person = new Person("Oleg", "Redka", date);
-        Console.WriteLine(person.ShowInfo());
-        Console.WriteLine();
-        ZNO zno = new ZNO("Math", 152, "Ukrainian", 157, "History", 163);
-        Applicant applicant = new Applicant("Oleg", "Redka", date, zno, 9.7, "School 28");
-        Console.WriteLine(applicant.ShowInfo());
-        Console.WriteLine();
-        Student student = new Student("Oleg", "Redka", date, zno, 9.7, "School 28", 1, "IPZ-23-2", "FICT", "ZPSU");
-        Console.WriteLine(student.ShowInfo());
-        Console.WriteLine();
-        DateTime date1 = new DateTime(1976, 5, 14);
-        Teacher teacher = new Teacher("Oleksii", "Chizhmotrya", date1, "Senior Lecturer", "Department of Software Engineering", "Zhytomyr Polytechnic State University");
-        Console.WriteLine(teacher.ShowInfo());
-        Console.WriteLine();
-        DateTime date2 = new DateTime(2024, 1, 6);
-        LibraryUser libraryUser = new LibraryUser("Oleg", "Redka", date, 4863753, date2, 24.50f);
-        Console.WriteLine(libraryUser.ShowInfo());
+        Person[] people = new Person[5];
+        people[0] = new Person("Oleg", "Redka", new DateTime(2006, 9, 27));
+        people[1] = new Applicant("Oleg", "Redka", new DateTime(2006, 9, 27), new ZNO("Math", 152, "Ukrainian", 157, "History", 163), 9.7, "School 28");
+        people[2] = new Student("Oleg", "Redka", new DateTime(2006, 9, 27), new ZNO("Math", 152, "Ukrainian", 157, "History", 163), 9.7, "School 28", 1, "IPZ-23-2", "FICT", "ZPSU");
+        people[3] = new Teacher("Oleksii", "Chizhmotrya", new DateTime(1976, 5, 14), "Senior Lecturer", "Department of Software Engineering", "Zhytomyr Polytechnic State University");
+        people[4] = new LibraryUser("Oleg", "Redka", new DateTime(2006, 9, 27), 4863753, new DateTime(2024, 1, 6), 24.50f);
+
+        for (int i = 0; i < people.Length; i++) 
+        {
+            Console.WriteLine(people[i].ShowInfo());
+            Console.WriteLine();
+        }
     }
 }
